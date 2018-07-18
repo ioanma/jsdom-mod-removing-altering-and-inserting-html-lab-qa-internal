23215efc15df852cf38830ecf0330e231706490e
Marios Ioannidis <ioanmar80@gmail.com>
2018-07-18 18:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 var element = document.createElement('div');
 document.body.appendChild(element);
-var ul = document.createElement('ul');
-element.appendChild(ul);
 
 
 /* Create your new element here and assign it to newHeader */
