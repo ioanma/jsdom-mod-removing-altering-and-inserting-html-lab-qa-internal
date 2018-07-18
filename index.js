1c151624751af6193c54d3a3759ca6e6dd7dd653
Marios Ioannidis <ioanmar80@gmail.com>
2018-07-18 18:53:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var element = document.createElement('div')
 document.body.appendChild(element)
 var ul = document.createElement('ul')
+element.appendChild(ul)
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
